0a056c3cb881225b07b98183d4768fe5dbff7fbd
nakamura0907 <nakamura990907@gmail.com>
2022-11-08 13:38:24 +0900

    app: ロギング機能

--- services/app/src/frameworks/webserver/middlewares/errorHandling.ts
+++ services/app/src/frameworks/webserver/middlewares/errorHandling.ts
@@ -1,12 +1,14 @@
 import type { Express, NextFunction, Request, Response } from "express";
 import Serializer from "@util/serializer";
 import Exception from "@util/exception/Exception";
+import logger from "@util/logger";
 
 const errorHandling = (app: Express) => {
   app.use((error: Exception, _: Request, res: Response, __: NextFunction) => {
     const statusCode = error.status || 500;
     const serializer = new Serializer();
 
+    logger.error(error.message);
     res.status(statusCode).send(serializer.error(error));
   });
 };
