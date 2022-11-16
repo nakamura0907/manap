fc6e26313cb782c3babc36bd46421887d718eef4
nakamura0907 <nakamura990907@gmail.com>
2022-11-16 09:40:27 +0900

    プロジェクト更新 権限確認なしVer

--- services/app/src/features/core/projects/controller.ts
+++ services/app/src/features/core/projects/controller.ts
@@ -3,6 +3,7 @@ import Project from "./domain/model/Project";
 import { Request, Response, NextFunction } from "express";
 import { GeneratedId } from "@/features/shared/Id";
 import { projectsQueryService, projectsRepository } from "@/container";
+import ProjectDetail from "./domain/model/ProjectDetail";
 
 type ProjectController = {
   /**
@@ -17,16 +18,21 @@ type ProjectController = {
    * プロジェクトIDからプロジェクトを取得
    */
   fetchById: (req: Request, res: Response, next: NextFunction) => void;
+  /**
+   * プロジェクトIDからプロジェクトを更新
+   */
+  update: (req: Request, res: Response, next: NextFunction) => void;
 };
 
 const projectController = (): ProjectController => {
   const create = (req: Request, res: Response, next: NextFunction) => {
     (async () => {
-      const { name, description } = req.body;
       const userId = req.user?.id;
+      const { name, description } = req.body;
 
       if (!userId) throw new Exception("認証に失敗しました", 401);
 
+      // 新規プロジェクト作成
       const validatedProject = Project.create(
         new GeneratedId(userId),
         name,
@@ -48,6 +54,7 @@ const projectController = (): ProjectController => {
 
       if (!userId) throw new Exception("認証に失敗しました", 401);
 
+      // プロジェクト一覧取得
       const result = await projectsQueryService.fetchList(userId);
 
       res.status(200).send({
@@ -67,8 +74,10 @@ const projectController = (): ProjectController => {
 
       if (!userId) throw new Exception("認証に失敗しました", 401);
 
+      // プロジェクト取得
       const projectId = GeneratedId.validate(Number(reqProjectId) || -1);
       const result = await projectsQueryService.fetchById(projectId.value);
+
       if (!result.members.find((member) => member.userId === userId))
         throw new Exception("プロジェクトに参加していません", 403);
 
@@ -81,7 +90,36 @@ const projectController = (): ProjectController => {
     })().catch(next);
   };
 
-  return { create, fetchById, fetchList };
+  const update = (req: Request, res: Response, next: NextFunction) => {
+    (async () => {
+      const reqProjectId = req.params.id;
+      const userId = req.user?.id;
+      const { name, description } = req.body;
+
+      if (!userId) throw new Exception("認証に失敗しました", 401);
+
+      // 権限があるか確認
+      /**
+       * TODO: 権限があるか確認する
+       * TODO: ミドルウェアとして分離する？？？
+       */
+      const projectId = GeneratedId.validate(Number(reqProjectId) || -1);
+
+      // プロジェクト更新
+      const validatedProject = ProjectDetail.create(name, description);
+      const project = validatedProject.setId(projectId);
+
+      await projectsRepository.update(project);
+
+      res.status(200).send({
+        id: project.id.value,
+        name: project.name.value,
+        description: project.description.value,
+      });
+    })().catch(next);
+  };
+
+  return { create, fetchById, fetchList, update };
 };
 
 export default projectController;
--- services/app/src/features/core/projects/domain/repository/IProjectsRepository.ts
+++ services/app/src/features/core/projects/domain/repository/IProjectsRepository.ts
@@ -1,8 +1,10 @@
-import { GeneratedId, NoneId } from "@/features/shared/Id";
 import Project from "../model/Project";
+import ProjectDetail from "../model/ProjectDetail";
+import { GeneratedId, NoneId } from "@/features/shared/Id";
 
 interface IProjectsRepository {
   create(project: Project<NoneId>): Promise<Project<GeneratedId>>;
+  update(project: ProjectDetail<GeneratedId>): Promise<void>;
 }
 
 export default IProjectsRepository;
