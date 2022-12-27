b21580eafdd830fc4a9f7860758ddd015ec6718d
nakamura0907 <nakamura990907@gmail.com>
2022-12-27 23:26:47 +0900

    プロジェクト画面レイアウト

--- services/web/src/components/template/layout/index.tsx
+++ services/web/src/components/template/layout/index.tsx
@@ -1,7 +1,7 @@
 import Avatar from "@components/ui/avatar";
 import Button from "@components/ui/button";
 import Dropdown from "@components/ui/dropdown";
-import Layout, { Content, Footer, Header } from "@components/ui/layout";
+import Layout, { Content, Footer, Header, Sider } from "@components/ui/layout";
 import Link from "@components/ui/link";
 import Menu from "@components/ui/menu";
 import React from "react";
@@ -65,34 +65,77 @@ export const ProjectPageLayout: React.FC<React.PropsWithChildren> = ({
   if (!projectId) return <></>;
 
   return (
-    <div>
-      <div>
-        <p>Layout.tsx test link</p>
-        <ul>
-          <li>
-            <Link href={`/projects/${projectId}`}>ダッシュボード</Link>
-          </li>
-          <li>
-            <Link href={`/projects/${projectId}/tasks`}>タスクボード</Link>
-          </li>
-          <li>
-            <Link href={`/projects/${projectId}/gantt_charts`}>
-              ガントチャート
-            </Link>
-          </li>
-          <li>
-            <Link href={`/projects/${projectId}/rooms`}>掲示板</Link>
-          </li>
-        </ul>
-      </div>
-      <BaseLayout>{children}</BaseLayout>
-    </div>
+    <BaseLayout
+      sider={
+        <Sider>
+          <Menu
+            theme="dark"
+            items={[
+              {
+                key: "ダッシュボード",
+                label: (
+                  <Link href={`/projects/${projectId}/`}>ダッシュボード</Link>
+                ),
+              },
+              {
+                key: "メンバー",
+                label: (
+                  <Link href={`/projects/${projectId}/members`}>メンバー</Link>
+                ),
+              },
+              {
+                key: "タスクボード",
+                label: (
+                  <Link href={`/projects/${projectId}/tasks`}>
+                    タスクボード
+                  </Link>
+                ),
+              },
+              {
+                key: "ガントチャート",
+                label: (
+                  <Link href={`/projects/${projectId}/gantt_charts`}>
+                    ガントチャート
+                  </Link>
+                ),
+              },
+              {
+                key: "機能提案",
+                label: (
+                  <Link href={`/projects/${projectId}/feature_suggestions`}>
+                    機能提案
+                  </Link>
+                ),
+              },
+              {
+                key: "掲示板",
+                label: (
+                  <Link href={`/projects/${projectId}/rooms`}>掲示板</Link>
+                ),
+              },
+              {
+                key: "設定",
+                label: (
+                  <Link href={`/projects/${projectId}/settings`}>設定</Link>
+                ),
+              },
+            ]}
+          />
+        </Sider>
+      }
+    >
+      {children}
+    </BaseLayout>
   );
+};
+
+type BaseLayout = React.PropsWithChildren & {
+  sider?: React.ReactNode;
 };
 
 const useAuth = () => React.useContext(authContext);
 const useSetAuth = () => React.useContext(setAuthContext);
-const BaseLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+const BaseLayout: React.FC<BaseLayout> = ({ children, sider }) => {
   const auth = useAuth();
   const setAuth = useSetAuth();
   const router = useRouter();
@@ -108,63 +151,70 @@ const BaseLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
 
   return (
     <Layout className="min-h-screen" style={{ backgroundColor: "#F3F8FF" }}>
-      <Header className="flex px-12 bg-white">
-        <div className="mr-auto">
-          <h1>
-            <Link href="/" className="text-black font-bold">
-              Manap
-            </Link>
-          </h1>
-        </div>
-        {!auth ? (
-          <Menu
-            mode="horizontal"
-            items={[
-              {
-                key: "ログイン",
-                label: <Link href="/login">ログイン</Link>,
-              },
-              {
-                key: "アカウント作成",
-                label: <Link href="/signup">アカウント作成</Link>,
-              },
-            ]}
-            className="w-auto"
-          ></Menu>
-        ) : (
-          <Dropdown
-            menu={{
-              items: [
+      {sider && <>{sider}</>}
+      <Layout>
+        <Header className="flex px-12 bg-white">
+          <div className="mr-auto">
+            <h1>
+              <Link href="/" className="text-black font-bold">
+                Manap
+              </Link>
+            </h1>
+          </div>
+          {!auth ? (
+            <Menu
+              mode="horizontal"
+              items={[
                 {
-                  key: "mypage",
-                  label: <Link href="/mypage">マイページ</Link>,
+                  key: "ログイン",
+                  label: <Link href="/login">ログイン</Link>,
                 },
                 {
-                  type: "divider",
+                  key: "アカウント作成",
+                  label: <Link href="/signup">アカウント作成</Link>,
                 },
-                {
-                  key: "ログアウト",
-                  label: (
-                    <Button type="text" onClick={handleLogout} className="p-0">
-                      ログアウト
-                    </Button>
-                  ),
-                },
-              ],
-            }}
-          >
-            <a onClick={(e) => e.preventDefault()}>
-              <Avatar icon={<UserOutlined />} />
-            </a>
-          </Dropdown>
-        )}
-      </Header>
-      <Content className="mx-12 my-14 p-10 bg-white">
-        <div className="max-w-screen-lg mx-auto">{children}</div>
-      </Content>
-      <Footer className="text-center bg-inherit">
-        &copy; All rights reserved.
-      </Footer>
+              ]}
+              className="w-auto"
+            />
+          ) : (
+            <Dropdown
+              menu={{
+                items: [
+                  {
+                    key: "mypage",
+                    label: <Link href="/mypage">マイページ</Link>,
+                  },
+                  {
+                    type: "divider",
+                  },
+                  {
+                    key: "ログアウト",
+                    label: (
+                      <Button
+                        type="text"
+                        onClick={handleLogout}
+                        className="p-0"
+                      >
+                        ログアウト
+                      </Button>
+                    ),
+                  },
+                ],
+              }}
+            >
+              <a onClick={(e) => e.preventDefault()}>
+                <Avatar icon={<UserOutlined />} />
+              </a>
+            </Dropdown>
+          )}
+        </Header>
+        <Content className="mx-12 my-14 p-10 bg-white">
+          <div className="max-w-screen-lg mx-auto">{children}</div>
+        </Content>
+        <Footer className="text-center bg-inherit">
+          &copy; All rights reserved.
+        </Footer>
+      </Layout>
     </Layout>
   );
 };
